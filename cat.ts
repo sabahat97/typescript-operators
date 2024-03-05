dfd9ee4abb4b2273c2014f5a2f95dd948591b80e
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-03-05 22:12:21 +0500

    Add files via upload

---
+++ cat.ts
@@ -0,0 +1,251 @@
+                               //        operators   \\
+                            
+                             //   10 lines assignment of each operator
+                            //          addition                  
+let  a  :  number  = 10 ;
+let  b  :  number  =  2 ;  
+  
+let sum = a + b ;
+               console.log (sum);   
+
+
+let sum0  :  number  = 34 ;       
+let sum1  :  number  = 24 ; 
+
+ let  totall = sum0 + sum1;
+                console.log (totall);
+
+
+let summ  :  number  = 12;
+let summ1 :  number  = 12;
+
+  let tota11 : number  = summ + summ1 ;
+                 console.log (tota11);
+
+
+let add   :  number  = 6 ;
+let add1  :  number  = 9 ;  
+  
+  let addition = add + add1 ;
+               console.log (addition);                 
+               
+
+let  num  :  number  = 10 ;
+let  num1 :  number  =  2 ;  
+  
+  let summ2 = num + num1 ;
+               console.log (summ2);                  
+               
+
+
+let  a1  :  number  = 10 ;
+let  b2  :  number  =  12 ;  
+  
+  let ab3 = a1 + b2 ;
+               console.log (ab3);                  
+
+
+let  a0  :  number  = 19 ;
+let  b1  :  number  = 19 ;  
+  
+  let ab0 = a0 + b1 ;
+               console.log (ab0);    
+               
+               
+
+let  numm   :  number  = 29 ;
+let  numm1  :  number  = 13 ;  
+  
+  let number = numm + numm1 ;
+               console.log (number);       
+               
+               
+let  c   :  number  = 20 ;
+let  d   :  number  = 19 ;  
+  
+  let cd = c + d ;
+               console.log (cd);      
+               
+               
+let  c1  :  number  = 66 ;
+let  d1  :  number  = 13 ;  
+  
+  let cd1 = c1 + d1 ;
+               console.log (cd1);                     
+               
+
+
+
+                     //   10 lines assignment of subtraction   \\
+
+
+let  subtraction  :  number  =  18;           
+let  subtraction1 :  number  =   9;
+let  subtraction2 :  number  =  subtraction - subtraction1;
+        console.log (subtraction2);
+
+
+
+let  subt   :  number  =  77;           
+let  subtr  :  number  =  66;
+let  subtr1 :  number  =  subt - subtr;
+        console.log (subtr1);
+        
+
+let  subt1  :  number  =  34;           
+let  subt2  :  number  =  16;
+let  sub1   :  number  =  subt1 - subt2;
+        console.log (sub1);
+        
+
+let  subt0  :  number  =  13;           
+let  subtr3 :  number  =  12;
+let  sub03  :  number  =  subt0 - subtr3;
+        console.log (sub03);        
+
+
+let  sub   :  number  =  43;           
+let  sub2  :  number  =  12;
+let  sub3  :  number  =  sub - sub2;
+        console.log (sub3);   
+        
+
+let  minus   :  number  =  66;           
+let  minus1  :  number  =  44;
+let  minus2  :  number  =  minus - minus1;
+        console.log (minus2);   
+
+ 
+let  ab      :  number  =  13;           
+let  bd      :  number  =  5 ;
+let  ad      :  number  =  ab - bd;
+        console.log (ad);     
+        
+        
+let  min     :  number  =  38;           
+let  minu    :  number  =  18;
+let  minuss  :  number  =  min - minu;
+        console.log (minuss);         
+
+
+let  mins     :  number  =  91;           
+let  mins1    :  number  =  82;
+let  mins3    :  number  =  mins - mins1;
+        console.log (mins3);          
+
+
+let  mint     :  number  =  45;           
+let  mints    :  number  =  14;
+let  tottall  :  number  =  mint - mints;
+        console.log (tottall);            
+
+
+
+         // assignment of multiplication 
+
+
+
+let  multi   :  number  =  2;
+let  multi1  :  number  =  2;      
+       console.log (multi * multi1);
+
+
+
+let  multi2   :  number  =  3;
+let  multi3   :  number  =  2;      
+       console.log (multi2 * multi3);   
+     
+     
+let  mul   :  number  =  5;
+let  mul1  :  number  =  5;      
+       console.log (mul * mul1);      
+
+
+let  mul0   :  number  =  8;
+let  mul2   :  number  =  5;      
+       console.log (mul0 * mul2);    
+
+
+let  m1   :  number  =  2;
+let  m2   :  number  =  9;      
+       console.log (m1 * m2);       
+
+
+let  m10   :  number  =  3;
+let  m20   :  number  =  7;      
+        console.log (m10 * m20);            
+
+
+let  m100   :  number  =  7;
+let  m200   :  number  =  9;      
+        console.log (m100 * m200);   
+     
+     
+let  m11   :  number  =  16;
+let  m22   :  number  =  2 ;      
+        console.log (m11 * m22);  
+     
+     
+let  m01    :  number  =  12;
+let  m02    :  number  =  1 ;      
+         console.log (m01 * m02);    
+
+     
+let  m03    :  number  =  10;
+let  m04    :  number  =  10;      
+         console.log (m03 * m04);         
+
+
+
+
+             // assignment of  division 
+
+
+let  div   :  number  = 12;             
+let  div1  :  number  =  2;
+         console.log (div / div1);
+
+
+let  div0   :  number  = 77;             
+let  div2   :  number  =  3;
+         console.log (div0 / div2);         
+
+
+let  divi   :  number  = 17;             
+let  divi1  :  number  =  4;
+         console.log (divi / divi1);         
+
+
+let  divi0   :  number  = 15;             
+let  divi3   :  number  =  2;
+         console.log (divi0 / divi3);  
+         
+         
+let  di    :  number  = 88;             
+let  di1   :  number  =  4;
+         console.log (di / di1);     
+         
+         
+let  di2   :  number  = 8;             
+let  di3   :  number  = 2;
+         console.log (di2 / di3);    
+         
+         
+let  divis   :  number  = 22;             
+let  divid   :  number  =  5;
+         console.log (divis / divid);     
+         
+         
+let  divided    :  number  = 6;             
+let  division   :  number  = 2;
+         console.log (divided / division);         
+
+
+let  divi4   :  number  = 13;             
+let  divi5   :  number  =  2;
+         console.log (divi4 / divi5);
+         
+         
+let  d0   :  number  =  66;             
+let  d9   :  number  =  6;
+         console.log (d0 / d9);         
